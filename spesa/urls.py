3bea8e262303251e95026963b93d6145845ab942
superqwe <superqwe@users.noreply.github.com>
2017-10-13 12:12:04 +0200

    modificato vista index

--- spesa/urls.py
+++ spesa/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from spesa import views
 
 urlpatterns = [
+    url(r'(?P<azione>^acquistato|^eliminato|^riacquista)/(?P<pk>[0-9]+)$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
-    # url(r'^spesa/$', views.index, name='index'),
 ]
--- spesa/views.py
+++ spesa/views.py
@@ -7,8 +7,25 @@ from django.shortcuts import render
 from spesa.models import Acquisto
 
 
-def index(request):
-    da_comprare = Acquisto.objects.filter(stato = '2')
-    comprato = Acquisto.objects.filter(stato = '1')
+def index(request, azione=None, pk=None):
+    if pk:
+        acquisto = Acquisto.objects.get(pk=pk)
+
+        if azione == 'acquistato':
+            acquisto.stato = 1
+
+        elif azione == 'eliminato':
+            acquisto.stato = 0
+
+        elif azione == 'riacquista':
+            acquisto.stato = 2
+
+        acquisto.save()
+
+    da_comprare = Acquisto.objects.filter(stato='2')
+    comprato = Acquisto.objects.filter(stato='1')
+    n_da_comprare = len(da_comprare)
+    n_comprato = len(comprato)
+    n_totale = n_da_comprare + n_comprato
 
     return render(request, 'spesa/lista_prodotti.html', locals())
